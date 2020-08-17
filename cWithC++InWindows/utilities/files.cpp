b3bb459b12dbae2e94bd3edf303b6c923adda0bf
qiaohairui <shecannotsee@163.com>
2020-08-17 15:09:41 +0800

    1.添加通用用户指南

--- cWithC++InWindows/utilities/files.cpp
+++ cWithC++InWindows/utilities/files.cpp
@@ -8,18 +8,32 @@ void dofile(char *in_fname,char *pwd,char *out_fname);/*对文件进行加密的具体函数
 
 /*定义main()函数的命令行参数*/
 int main(int argc,char *argv[]){
+    printf("使用指南（加密解密均使用该软件）：\n");
+    printf("小明有一个文件 yuan_shi_wen_jian.mp4 需要加密。\n");
+    printf("首先他输入 yuan_shi_wen_jian.mp4\n");
+    printf("然后他输入加密密码123456\n");
+    printf("然后他输入加密后的文件名 jia_mi_hou_wen_jian.mp4\n");
+    printf("小红获得加密后文件之后，输入文件名 jia_mi_hou_wen_jian.mp4\n");
+    printf("然后输入和小明约定好的加密解密密码123456n");
+    printf("然后生成解密后的文件，也就是小明最开始的源文件 xiao_hong.mp4\n");
+    printf("最后小红获得最开始的文件“yuan_shi_wen_jian.mp4”\n");
+    printf("\n\n");
+
     char in_fname[30];/*用户输入的要加密的文件名*/
     char out_fname[30];
     char pwd[8];/*用来保存密码*/
     
     if(argc!=4){/*容错处理*/
-        printf("In-fname:");
+        printf("请输入要加密（解密）的文件名（最好与程序放在用一个目录下，并且加上文件的后缀，文件名不要超过三十个字）：");
+        //printf("In-fname:");
         std::cin>>in_fname;/*得到要加密的文件名*/
         
-        printf("Password:");
+        printf("请输入加密密码（不超过八位）：");
+        //printf("Password:");
         std::cin>>pwd;/*得到密码*/
         
-        printf("Out-file:");
+        printf("请输入生成的文件名：");
+        //printf("Out-file:");
         std::cin>>out_fname;/*得到加密后你要的文件名*/
         
         
@@ -43,12 +57,14 @@ void dofile(char *in_fname,char *pwd,char *out_file){
     
     fp1=fopen(in_fname,"rb");
     if(fp1==NULL){
-        printf("cannot open in-file./n");   
+        printf("打不开软件./n"); 
+        //printf("cannot open in-file./n");   
         exit(1);/*如果不能打开要加密的文件,便退出程序*/
     }
     fp2=fopen(out_file,"wb");
     if(fp2==NULL){
-        printf("cannot open or create out-file./n");
+        printf("不能建立加密后的文件./n"); 
+        //printf("cannot open or create out-file./n");
         exit(1);/*如果不能建立加密后的文件,便退出*/
     }
     while(pwd[++j0]);
